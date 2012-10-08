f174c8ee87fb1e617b29d02a1ca216a19560ae31
Carl-Anton Ingmarsson <ca.ingmarsson@gmail.com>
2012-10-08 15:21:01 +0200

    Reindent

--- F.java
+++ F.java
@@ -1,29 +1,29 @@
 class F {
-    public static String format(String s, int len) {
-    	
-    	StringBuilder b = new StringBuilder();
-    	int slen = len-s.length();
+	public static String format(String s, int len) {
 
-    	for (int i = 0; i < slen; i++)
-    		b.append(' ');
-    	
-    	b.append(s);
-    	return b.toString();
-    }
+		StringBuilder b = new StringBuilder();
+		int slen = len-s.length();
 
-    public static String format(Object x, int len){
-	return format(String.valueOf(x), len);
-    }
+		for (int i = 0; i < slen; i++)
+			b.append(' ');
 
-    public static String format(long x, int len){
-	return format(String.valueOf(x), len);
-    }
+		b.append(s);
+		return b.toString();
+	}
 
-    public static String format(double x, int len){
-	return format(String.valueOf(x), len);
-    }
+	public static String format(Object x, int len){
+		return format(String.valueOf(x), len);
+	}
 
-    public static String format(char x, int len){
-	return format(String.valueOf(x), len);
-    }
+	public static String format(long x, int len){
+		return format(String.valueOf(x), len);
+	}
+
+	public static String format(double x, int len){
+		return format(String.valueOf(x), len);
+	}
+
+	public static String format(char x, int len){
+		return format(String.valueOf(x), len);
+	}
 }
